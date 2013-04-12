e7c028ac0f5209fb531f666b7eae8a55a967b1f8
Ben Kiefer <kieferfam@msn.com>
2013-04-12 12:46:12 -0500

    Ben: adding logging and a property file

--- src/main/java/org/burgers/email/service/EmailSender.java
+++ src/main/java/org/burgers/email/service/EmailSender.java
@@ -1,10 +1,14 @@
 package org.burgers.email.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class EmailSender {
+    public static final Logger logger = LoggerFactory.getLogger(EmailSender.class);
     public void doSomething(){
-        // todo: wait for it
+        logger.info("logging works");
     }
 }
---
+++ src/test/java/org/burgers/email/service/EmailSenderTest.java
@@ -0,0 +1,10 @@
+package org.burgers.email.service;
+
+import org.junit.Test;
+
+public class EmailSenderTest {
+    @Test
+    public void doSomething() {
+        //todo: something
+    }
+}
