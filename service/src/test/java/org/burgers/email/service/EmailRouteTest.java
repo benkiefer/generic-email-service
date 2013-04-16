7a223ed59cde9b39edacd7ea9136e99ba211b075
Ben Kiefer <kieferfam@msn.com>
2013-04-16 11:11:52 -0500

    Ben: adding a failure to load template test

--- service/src/test/java/org/burgers/email/service/EmailRouteTest.java
+++ service/src/test/java/org/burgers/email/service/EmailRouteTest.java
@@ -6,7 +6,9 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.model.ToDefinition;
 import org.burgers.email.client.TemplateEmailRequest;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,7 +29,6 @@ import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@DirtiesContext
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:contexts/test-applicationContext.xml"})
 public class EmailRouteTest {
@@ -35,6 +36,9 @@ public class EmailRouteTest {
     public static final String FROM = "from@test.com";
     public static final String SUBJECT = "subject";
     private static final String NEW_FROM_URI = "direct:testing";
+
+    @EndpointInject(uri = "mock:result")
+    private MockEndpoint errorEndpoint;
 
     @Autowired
     private ModelCamelContext camelContext;
@@ -53,11 +57,21 @@ public class EmailRouteTest {
                 replaceFromWith(NEW_FROM_URI);
             }
         });
+
+        camelContext.getRouteDefinition("failureRoute").adviceWith(camelContext, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveByType(ToDefinition.class).selectLast().replace().to(errorEndpoint);
+            }
+        });
     }
 
     @Test
+    @DirtiesContext
     public void sendMessage() throws Exception {
-        template.sendBody(NEW_FROM_URI, new XStream().toXML(createFakeRequest()));
+        errorEndpoint.setExpectedMessageCount(0);
+
+        template.sendBody(NEW_FROM_URI, new XStream().toXML(createFakeRequest("test.template")));
 
         List<WiserMessage> messages = mailServer.getMessages();
 
@@ -70,14 +84,26 @@ public class EmailRouteTest {
         assertEquals(TO, asList(message.getHeader("To")).get(0));
         assertEquals(FROM, asList(message.getHeader("From")).get(0));
         assertEquals(SUBJECT, message.getSubject());
+        errorEndpoint.assertIsSatisfied();
     }
 
-    private TemplateEmailRequest createFakeRequest(){
+    @Test
+    @DirtiesContext
+    public void sendMessage_error() throws Exception {
+        errorEndpoint.setExpectedMessageCount(1);
+
+        template.sendBody(NEW_FROM_URI, new XStream().toXML(createFakeRequest("kaboom")));
+        assertEquals(0, mailServer.getMessages().size());
+
+        errorEndpoint.assertIsSatisfied();
+    }
+
+    private TemplateEmailRequest createFakeRequest(String templateName){
         TemplateEmailRequest request = new TemplateEmailRequest();
         request.setTo(asList(TO));
         request.setFrom(FROM);
         request.setSubject(SUBJECT);
-        request.setTemplateName("test.template");
+        request.setTemplateName(templateName);
         Map<String, String> propertyMap = new HashMap<String, String>();
         propertyMap.put("name", "Testing");
         request.setPropertyMap(propertyMap);
